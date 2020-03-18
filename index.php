a822b063d1404f7e774dfb4d4cda53d00b5454f7
Hugo Hirsch <github@hugo-hirsch.de>
2020-03-18 21:15:20 +0100

    Add copy2clipboard functionality

--- index.php
+++ index.php
@@ -6,13 +6,23 @@
 </head>
 <body>
 <h1>WhatIsMyIP?</h1>
-<?php
 
-echo '<h2>Your IP address is: 🔑 '.$_SERVER['REMOTE_ADDR'].'</h2>';
-echo '<h2>Your browser is: 🤖 '.$_SERVER['HTTP_USER_AGENT'].'</h2>';
+<script>
+    function copyToClipboard(text) {
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+    }
+</script>
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+echo '<h2>Your IP address is: 🔑 ' . $ip . '</p><h2/>';
+echo '<p>Copy to clipboard <button id="demo" onclick="copyToClipboard(document.getElementById(\'demo\').innerHTML)">' . $ip . '</button></p>';
+
+echo '<h2>Your browser is: 🤖 ' . $_SERVER['HTTP_USER_AGENT'] . '</h2>';
 
 echo '<hr/>';
-echo '<pre>Proudly served by <a href="https://www.github.com/ottlinger/whatismyip"i target="_blank">WhatIsMyIP</a> at '.date("Y-m-d H:i:s").'</pre>';
+echo '<pre>Proudly served by <a href="https://www.github.com/ottlinger/whatismyip"i target="_blank">WhatIsMyIP</a> at ' . date("Y-m-d H:i:s") . '</pre>';
 ?>
+
+
 </body>
 </html>
