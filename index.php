546845f6364da6e4e2724216f189955f0fd9ee10
Hugo Hirsch <github@hugo-hirsch.de>
2020-03-18 20:57:25 +0100

    Add HTML skeleton

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<meta charset="utf-8">
+<head>
+    <title>WhatIsMyIP &mdash; Show me please</title>
+</head>
+<body>
+<h1>WhatIsMyIP?</h1>
 <?php
 
-echo '<h1>Your IP address is: 🔑 '.$_SERVER['REMOTE_ADDR'].'</h2>';
-echo '<h1>Your browser is: 🤖 '.$_SERVER['HTTP_USER_AGENT'].'</h2>';
+echo '<h2>Your IP address is: 🔑 '.$_SERVER['REMOTE_ADDR'].'</h2>';
+echo '<h2>Your browser is: 🤖 '.$_SERVER['HTTP_USER_AGENT'].'</h2>';
 
 echo '<hr/>';
 echo '<pre>Proudly served by <a href="https://www.github.com/ottlinger/whatismyip"i target="_blank">WhatIsMyIP</a> at '.date("Y-m-d H:i:s").'</pre>';
+?>
+</body>
+</html>
