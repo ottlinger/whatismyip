04e6d5f6f58b8ca79c6de12e46f225238af17292
Hugo Hirsch <github@hugo-hirsch.de>
2020-03-18 20:48:32 +0100

    Add footer

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 echo '<h1>Your IP address is: '.$_SERVER['REMOTE_ADDR'].'</h2>';
 echo '<h1>Your browser is: '.$_SERVER['HTTP_USER_AGENT'].'</h2>';
 
+echo '<hr/>';
+echo '<pre>Proudly served by <a href="https://www.github.com/ottlinger/whatismyip"i target="_blank">WhatIsMyIP</a> at '.date("Y-m-d H:i:s").'</pre>
