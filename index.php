e09a101669cf2a5a0c54f30d7bc5da26589a3269
Hugo Hirsch <github@hugo-hirsch.de>
2021-03-23 14:21:42 +0100

    Change layout and log value to see if IPv6 is working

--- index.php
+++ index.php
@@ -14,14 +14,16 @@
         textArea.select();
         document.execCommand("Copy");
         textArea.remove();
+
+        console.log(textArea.value);
     }
 </script>
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
-echo '<h2>Your IP address is: 🔑 <span id=\'ipadr\'>'.$ip.'</span></h2>';
+echo '<h2>Your IP address is:</h2><h2>🔑 <span id=\'ipadr\'>'.$ip.'</span></h2>';
 echo '<p><button type="button" onclick="javascript:copyToClipboard()">COPY2Clipboard</button></p>';
 
-echo '<h2>Your browser is: 🤖 '.$_SERVER['HTTP_USER_AGENT'].'</h2>';
+echo '<h2>Your browser is:</h2><h2> 🤖 '.$_SERVER['HTTP_USER_AGENT'].'</h2>';
 
 echo '<hr/>';
 echo '<pre>Proudly served by <a href="https://www.github.com/ottlinger/whatismyip" target="_blank">WhatIsMyIP</a> at '.date('Y-m-d H:i:s').'</pre>';
